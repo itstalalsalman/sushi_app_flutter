c423fba95c65b347f043ec8c9a81b5bb7319dccc
Talal Salman Zafeer <mustangcreator@gmail.com>
2024-02-06 11:43:31 +0300

    Intro Page for the application implementing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_dev/pages/intro_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
+      home: IntroPage(),
     );
   }
 }
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 138, 60, 55),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          const SizedBox(height: 25,),
+          //shop name
+          Text("SUSHI TOWN", style: GoogleFonts.dmSerifDisplay(
+            fontSize: 30,
+            color: Colors.white,
+            ),
+          ),
+        
+          const SizedBox(height: 25,),
+
+          //icon
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Image.asset('lib/images/sushi-1.png'),
+          ),
+
+          const SizedBox(height: 25,),
+
+          //title
+          Text("THE TASTE OF JAPANESE FOOD", style: GoogleFonts.dmSerifDisplay(
+            fontSize: 44,
+            color: Colors.white,
+            ),
+          ),
+
+          const SizedBox(height: 10,),
+          //subtitle.
+          Text("Feel the taste of the most popular Japanese food, now within New York.",
+            style: TextStyle(
+              color: Colors.grey[300],
+              height: 2,
+            )
+          ),
+
+          const SizedBox(height: 25,),
+        
+          //get started button
+
+
+
+
+          
+        ],),
+      ),
+    );
+  }
+}
